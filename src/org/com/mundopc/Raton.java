c422604430b648abbf335a242ebbc90286210a74
Leonardo <leonardo38361@gmail.com>
2024-05-15 00:20:14 -0300

    clase Raton

--- src/org/com/mundopc/Raton.java
+++ src/org/com/mundopc/Raton.java
@@ -1,2 +1,19 @@
-package org.com.mundopc;public class Raton {
+package org.com.mundopc;
+
+public class Raton extends DispositivoEntrada {
+    private final int idRaton;
+    private static int contadorRatones;
+
+    public Raton(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idRaton = ++Raton.contadorRatones;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Raton{");
+        sb.append("idRaton=").append(idRaton);
+        sb.append('}');
+        return sb.toString();
+    }
 }
