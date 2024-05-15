7afba1c26f39e7efe5091de51710910f11f80ac0
Leonardo <leonardo38361@gmail.com>
2024-05-15 00:19:55 -0300

    clase Teclado

--- src/org/com/mundopc/Teclado.java
+++ src/org/com/mundopc/Teclado.java
@@ -1,2 +1,19 @@
-package org.com.mundopc;public class Teclado {
+package org.com.mundopc;
+
+public class Teclado extends DispositivoEntrada{
+    private final int idTeclado;
+    private static int contadorTeclados;
+
+    public Teclado(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclados;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Teclado{");
+        sb.append("idTeclado=").append(idTeclado);
+        sb.append('}');
+        return sb.toString();
+    }
 }
