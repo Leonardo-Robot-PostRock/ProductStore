a0603fb7de4fbbc78d7b9a9665c9d2c954778e1f
Leonardo <leonardo38361@gmail.com>
2024-05-15 00:21:21 -0300

    clase Orden

--- src/org/com/mundopc/Orden.java
+++ src/org/com/mundopc/Orden.java
@@ -1,2 +1,30 @@
-package org.com.mundopc;public class Orden {
+package org.com.mundopc;
+
+public class Orden {
+    private final int idOrden;
+    private Computadora computadoras[];
+    private int contadorComputadoras;
+    private static int contadorOrdenes;
+    private final static int MAX_COMPUTADORAS = 10;
+
+    public Orden() {
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.computadoras = new Computadora[Orden.MAX_COMPUTADORAS];
+    }
+
+    public void agregarComputadora(Computadora computadora) {
+        if (this.contadorComputadoras < Orden.MAX_COMPUTADORAS) {
+            this.computadoras[this.contadorComputadoras++] = computadora;
+        } else {
+            System.out.println("Has superado el límite de computadoras: " + Orden.MAX_COMPUTADORAS);
+        }
+    }
+
+    public void mostrarOrden() {
+        System.out.println("Id Orden: " + this.idOrden);
+        System.out.println("Dispositivos de la Orden: ");
+        for (int i = 0; i < this.contadorComputadoras; i++) {
+            System.out.println(this.computadoras[i].getNombre());
+        }
+    }
 }
